a155100a6c4285b6dce7b19ef12dde7403cc1a10
Valia Dempsey <valentinaml@gmail.com>
2017-12-15 11:04:31 -0800

    Add routes to Routes array in app.routing.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { VocabularyComponent } from './vocabulary/vocabulary.component';
 import { EditVocabularyComponent } from './edit-vocabulary/edit-vocabulary.component';
@@ -22,7 +22,8 @@ import { ResourcesComponent } from './resources/resources.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,30 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { VocabularyComponent } from './vocabulary/vocabulary.component';
+import { VocabularyDetailComponent } from './vocabulary-detail/vocabulary-detail.component';
+import { EditVocabularyComponent } from './edit-vocabulary/edit-vocabulary.component';
+import { ResourcesComponent } from './resources/resources.component';
 
-const appRoutes: Routes = [ ];
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'vocabulary',
+    component: VocabularyComponent
+  },
+  {
+    path: 'terms/:id',
+    component: VocabularyDetailComponent
+  },
+  // this will work after creating variable terms in terms.service.ts
+  {
+    path: 'resources',
+    component: ResourcesComponent
+  },
+
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
